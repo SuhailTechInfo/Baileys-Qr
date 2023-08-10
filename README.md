9fea9d17bf77eee37d831776a4cb58809a0fb053
Suhail Tech info <114290608+SuhailTechInfo@users.noreply.github.com>
2023-08-10 17:23:27 +0000

    sᴜʜᴀɪʟ-sᴇʀ-ʙᴀɪʟᴇʏs-ǫʀ

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# Baileys-Qr
+
+<h1 align="center">Baileys-Qr Made by SUHAIL TECH</h1>
+<p align="center"> Get Baileys Qr, where session id Converted into 'base64'. You can change it According to your needs. </p>
+
+<p align="center">
+  <a href="https://youtube.com/@suhailtechinfo">
+    <img alt="Suhail ser" height="400" src="https://i.imgur.com/NpA3ZsJ.jpeg">
+  </a>
+</p>
+
+  <html>
+   <body>
+  <p align="center">
+  <a href="https://wa.me/923184474176" target="_blank">
+    <img alt="whatsapp" src="https://img.shields.io/badge/ Whatsapp -25D366?style=for-the-badge&logo=whatsapp&logoColor=white" />
+  </a>
+  <a aria-label="Baleys_Qr free" href="https://github.com/SuhailTechInfo/Secktor-bot/fork" target="_blank">
+    <img alt="License: GPL-3" src="https://img.shields.io/github/stars/SuhailTechInfo/Baileys-Qr?style=social" target="_blank" />
+  </a>
+  <a aria-label="Suhail_Md is free to use" href="https://youtube.com/@suhailtechinfo" target="_blank">
+    <img alt="SuhailTechInfo Yt" src="https://img.shields.io/youtube/channel/subscribers/UCU071AMRqcd5mfTdCgJFwPg" target="_blank" />
+  </a>
+    <p align="center"><img src="https://profile-counter.glitch.me/{SuhailBaileysQr}/count.svg" alt="SuhailTechInfo :: Visitor's Count" /></p>
+
+     
+  </body>
+</html>
+
+## Features
+---
+1. ***WhatsApp Login***
+2. ***Session Generator***
+3. ***Bailey-library Login***
+4. ***No Encryption, Free fo Use***
+##
+
+
+
+
+## Installation
+---
+1.  ***Run on [`Replit`](https://replit.com/github/SuhailTechInfo/Baileys-Qr)***
+2. ***Run manually in any terminal Terminal, `TERMUX, VPS, RDP, etc.`***
+```
+$ git clone https://replit.com/github.com/SuhailTechInfo/Baileys-Qr.git
+$ cd suhail-qr
+$ npm i
+$ npm start
+```
+##
---
+++ index.js
@@ -0,0 +1,112 @@
+
+/*
+
+//══════════════════════════════════════════════════════════════════════════════════════════════════════//
+//                                                                                                      //
+//                                ＷＨＡＴＳＡＰＰ ＢＯＴ－ＭＤ ＢＥＴＡ                                      // 
+//                                                                                                      // 
+//                                         Ｖ：１．０．１                                                 // 
+//                                                                                                      // 
+//            ███████╗██╗   ██╗██╗  ██╗ █████╗ ██╗██╗         ███╗   ███╗██████╗                        //
+//            ██╔════╝██║   ██║██║  ██║██╔══██╗██║██║         ████╗ ████║██╔══██╗                       //
+//            ███████╗██║   ██║███████║███████║██║██║         ██╔████╔██║██║  ██║                       //
+//            ╚════██║██║   ██║██╔══██║██╔══██║██║██║         ██║╚██╔╝██║██║  ██║                       //
+//            ███████║╚██████╔╝██║  ██║██║  ██║██║███████╗    ██║ ╚═╝ ██║██████╔╝                       //
+//            ╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚══════╝    ╚═╝     ╚═╝╚═════╝                        //
+//                                                                                                      //
+//                                                                                                      //
+//                                                                                                      //
+//══════════════════════════════════════════════════════════════════════════════════════════════════════//
+
+CURRENTLY RUNNING ON BETA VERSION!!
+*
+   * @project_name : baileys-qr
+   * @author : Suhail Tech Info
+   * @youtube : https://www.youtube.com/SuhailTechInfo
+   * @description : Get baileys qr, where session id Converted into 'base64'. You can change it According to your needs.
+   * @version 1.0.0
+*
+   * Licensed under the  GPL-3.0 License;
+* 
+   * Created By Suhail Tech Info.
+   * © 2023 Suhail-Md.
+*/
+
+const fs   = require("fs-extra");
+const pino = require("pino");
+let qrcode = require("qrcode-terminal");
+
+
+if (fs.existsSync('./auth_info_baileys')) {
+  fs.emptyDirSync(__dirname + '/auth_info_baileys');
+  require('child_process').exec('rm -rf auth_info_baileys')
+  console.log('\nPlease Wait... Removing Cache files');
+  setTimeout(() => {   console.log(' ')    }, 100);
+  setTimeout(() => {   console.log('Q')    }, 300);
+  setTimeout(() => {   console.log('R')    }, 500);
+  setTimeout(() => {   console.log(' ')    }, 700);
+  setTimeout(() => {   console.log('B')    }, 900);
+  setTimeout(() => {   console.log('Y')    }, 1100);
+  setTimeout(() => {   console.log(' ')    }, 1300);
+  setTimeout(() => {   console.log('S')    }, 1500);
+  setTimeout(() => {   console.log('U')    }, 1700);
+  setTimeout(() => {   console.log('H')    }, 1900);
+  setTimeout(() => {   console.log('A')    }, 2100);
+  setTimeout(() => {   console.log('I')    }, 2300);
+  setTimeout(() => {   console.log('L')    }, 2500);
+  setTimeout(() => {   console.log(' ')    }, 2700);
+  setTimeout(() => {   console.log('Cache Cleared..!\nRun The Script Again')    }, 2900);
+  setTimeout(() => {   process.exit()      }, 3000)
+};
+setTimeout(() => {
+  const { default: makeWASocket, useMultiFileAuthState, Browsers, delay, makeInMemoryStore, } = require("@sampandey001/baileys");
+  const store = makeInMemoryStore({ logger: pino().child({ level: 'silent', stream: 'store' }) })
+  async function sᴜʜᴀɪʟ_ʙᴀɪʟᴇʏs_ǫʀ() {
+    const { state, saveCreds } = await useMultiFileAuthState(__dirname + '/auth_info_baileys')
+    try {
+      let session = makeWASocket({ printQRInTerminal: true, logger: pino({ level: "silent" }), browser: Browsers.macOS("Desktop"), auth: state });
+      session.ev.on("connection.update", async (s) => {
+        const { connection, lastDisconnect, qr } = s;
+        if (connection == "open")
+        {
+          await delay(500);
+          let user = session.user.id;         // User = Number of that user who just Scanned the Qr
+
+//===========================================================================================
+//===============================  SESSION ID   =============================================
+//===========================================================================================
+          let unique = fs.readFileSync(__dirname + '/auth_info_baileys/creds.json') //GETTING CREDS FROM CREDS.json TO GENERATE SESSION ID 
+          c = Buffer.from(unique).toString('base64'); // CHANGE 'base64' ACCORDING TO YOUR NEEDS 
+          console.log(`
+====================  SESSION ID  ===========================                   
+SESSION-ID ==> ${c}\n\n
+Don't provide your SESSION_ID to anyone otherwise that user can access your account.
+Thanks.
+-------------------  SESSION CLOSED   -----------------------
+`)   
+
+let cc = `╔════◇
+║『 *THANKS FOR CHOOSING SUHAIL-MD* 』
+║ _You complete first step to making Bot._
+╚════════════════════════╝
+╔═════◇
+║  『••• 𝗩𝗶𝘀𝗶𝘁 𝗙𝗼𝗿 𝗛𝗲𝗹𝗽 •••』
+║ *1.Github:* _github.com/suhailtechinfo_
+║ *2.Ytube:* _youtube.com/suhailtechinfo_
+║ *3.Owner:* _https://wa.me/923184474176_
+║ *Note :*_Don't provide your SESSION_ID to_
+║ _anyone otherwise that can access chats_
+╚════════════════════════╝
+`;
+          let session_id = await session.sendMessage(user, { text: c });      //SENDING 'base64' SESSION ID TO USER NUMBER
+          await session.sendMessage(user, { text: cc } , { quoted : session_id });
+          await require('child_process').exec('rm -rf auth_info_baileys')     //CLEAR 'auth_info_baileys' SO THAT NEXT TIME IT CLEARED FOR SCANNING
+          process.exit(1)   // STOPPING PROCESS AFTER GETTING SESSION ID
+        }
+        session.ev.on('creds.update', saveCreds)
+       if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) { sᴜʜᴀɪʟ_ʙᴀɪʟᴇʏs_ǫʀ(); } // IF ANY ERROR< THEN PRINT QR AGAIN
+      });
+    } catch (err) {console.log(err);await require('child_process').exec('rm -rf auth_info_baileys');process.exit(1);}
+  }
+  sᴜʜᴀɪʟ_ʙᴀɪʟᴇʏs_ǫʀ();
+}, 3000)
